a665c78a2a0c70a4af760375bb9d2de2154df23c
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-09 10:32:23 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,5 +1,5 @@
 import glob
-from userbot import Andencento
+from userbot import Andencento as bot
 from sys import argv
 import sys
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
@@ -18,25 +18,25 @@ import telethon.utils
 
                     
 async def add_bot(bot_token):
-    await Andencento.start(bot_token)
-    Andencento.me = await Andencento.get_me() 
-    Andencento.uid = telethon.utils.get_peer_id(Andencento.me)
+    await bot.start(bot_token)
+    bot.me = await bot.get_me() 
+    bot.uid = telethon.utils.get_peer_id(Andencento.me)
 
 
 
 if len(sys.argv) not in (1, 3, 4):
-    Andencento.disconnect()
+    bot.disconnect()
 else:
-    Andencento.tgbot = None
+    bot.tgbot = None
     try:
         if Config.BOT_USERNAME is not None:
             LOGS.info("Checking Telegram Bot Username...")
-            Andencento.tgbot = TelegramClient(
+            bot.tgbot = TelegramClient(
                 "BOT_TOKEN", api_id=Config.APP_ID, api_hash=Config.API_HASH
             ).start(bot_token=Config.BOT_TOKEN)
             LOGS.info("Checking Completed. Proceeding to next step...")
             LOGS.info(" Starting Andencento")
-            Andencento.loop.run_until_complete(Andencento_bot(Config.BOT_USERNAME))
+            bot.loop.run_until_complete(Andencento_bot(Config.BOT_USERNAME))
             LOGS.info(" Andencento Startup Completed")
         else:
-            Andencento.start()
+            bot.start()
