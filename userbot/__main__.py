a5f4f7c46a2b2dd7fdc5cdc09a729b92fe5b3e78
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-07-09 10:13:35 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -42,18 +42,6 @@ else:
             Andencento.start()
 
 
-
-# imports plugins...
-path = "userbot/plugins/*.py"
-files = glob.glob(path)
-for name in files:
-    with open(name) as f:
-        path1 = Path(f.name)
-        shortname = path1.stem
-        load_module(shortname.replace(".py", ""))
-
-
-
 print("Andencento Deployed And Working Fine For Assistance")
 
 
