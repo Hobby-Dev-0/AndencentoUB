9afd6b60972dcf890ab531a2e3e49ea5be7440b6
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-11 18:47:49 +0530

    ok

---
+++ userbot/assistant/__init__.py
@@ -0,0 +1,19 @@
+
+from .. import *
+from ..config import Config
+from ..helpers import *
+from ..utils import *
+from telethon import version
+
+from telethon import version
+from telethon.errors.rpcerrorlist import (
+    MediaEmptyError,
+    WebpageCurlFailedError,
+    WebpageMediaEmptyError,
+)
+from telethon.events import CallbackQuery
+
+from userbot import *
+from userbot.config import Config
+from userbot.helpers import *
+from userbot.utils import *
