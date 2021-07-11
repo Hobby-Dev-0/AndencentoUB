cd7a2828b5ac8fb7da1a501e9255212af0b2dd5f
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-11 13:04:52 +0530

    Uptime

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -13,6 +13,8 @@ from userbot.utils import *
 from userbot.random_strings import *
 from telethon import version
 
+uptime = f"{str(datetime.datetime.now()
+
 Eiva_USER = Andencento.me.first_name
 ForGo10God = Andencento.uid
 Eiva_mention = f"[{Eiva_USER}](tg://user?id={ForGo10God})"
