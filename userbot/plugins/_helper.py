08d19da8905cf53c0f3d1ce22fea8d81152243fc
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-12 14:07:43 +0530

    bt use

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -4,7 +4,7 @@ from . import CMD_LIST
 @command(pattern="^.ehelp ?(.*)")
 async def cmd_list(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
+        tgbotusername = Config.BOT_USERNAME
         input_str = event.pattern_match.group(1)
         if tgbotusername is None or input_str == "text":
             string = ""
