68b8344687fcf38cd6548cf91efbe36649050405
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-12 15:15:37 +0530

    inir

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -5,4 +5,4 @@ from .funcs import *
 from .extras import *
 from .assistant_load import *
 from ..helpers.progress import *
-from .main import command, admin_cmd, sudo_cmd, is_admin, register
+from .main import command, is_admin, register
