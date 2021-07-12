e4ba74b63b6ac225d3a5242741c551884c4c068b
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-12 09:48:42 +0530

    nnob

--- userbot/plugins/auto_profile.py
+++ userbot/plugins/auto_profile.py
@@ -11,30 +11,91 @@ DEFAULTUSERBIO = Config.BIO_MSG
 DEL_TIME_OUT = 60
 
 
-@Andencento.on(admin_cmd(pattern="autoname"))
+import asyncio
+import time
+
+from telethon.errors import FloodWaitError
+from telethon.tl import functions
+from . import *
+
+DEFAULTUSERBIO = str(BIO_MSG) if BIO_MSG else "ʟɛɢɛռɖaʀʏ ᴀғ ɦɛʟʟɮօt"
+DEL_TIME_OUT = 60
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Hell User"
+
+
+@Andencento.on(admin_cmd(pattern="autoname"))  # pylint:disable=E0602
+async def _(event):
+    noob = await edit_or_reply(event, "`Starting AutoName Please Wait`")
+    if event.fwd_from:
+        return
+
+    while True:
+
+        HB = time.strftime("%d-%m-%y")
+
+        HE = time.strftime("%H:%M")
+
+        name = f"🕒{HE} ⚡{DEFAULTUSER}⚡ 📅{HB}"
+
+        logger.info(name)
+
+        try:
+
+            await borg(
+                functions.account.UpdateProfileRequest(  # pylint:disable=E0602
+                    first_name=name
+                )
+            )
+
+        except FloodWaitError as ex:
+
+            logger.warning(str(e))
+
+            await asyncio.sleep(ex.seconds)
+
+        # else:
+
+        # logger.info(r.stringify())
+
+        # await borg.send_message(  # pylint:disable=E0602
+
+        #     Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
+
+        #     "Successfully Changed Profile Name"
+
+        # )
+
+        await asyncio.sleep(DEL_TIME_OUT)
+
+    await noob.edit(f"Auto Name has been started my Master")
+
+
+@Andencento.on(admin_cmd(pattern="autobio"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
-    user = await edit_or_reply(event, "`Starting AutoName Please Wait`")
-    if event.fwd_from:
-        return
     while True:
-        HB = time.strftime("%d-%m-%y")
-        HE = time.strftime("%H:%M")
-        name = f"🕒{HE} ⚡{Andencento_USER}⚡ 📅{HB}"
-        logger.info(name)
+        DMY = time.strftime("%d.%m.%Y")
+        HM = time.strftime("%H:%M:%S")
+        bio = f"📅 {DMY} | {DEFAULTUSERBIO} | ⌚️ {HM}"
+        logger.info(bio)
         try:
-            await bot(
+            await borg(
                 functions.account.UpdateProfileRequest(  # pylint:disable=E0602
-                    first_name=name
+                    about=bio
                 )
             )
         except FloodWaitError as ex:
             logger.warning(str(e))
             await asyncio.sleep(ex.seconds)
+        # else:
+        # logger.info(r.stringify())
+        # await borg.send_message(  # pylint:disable=E0602
+        # Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
+        # "Successfully Changed Profile Bio"
+        # )
         await asyncio.sleep(DEL_TIME_OUT)
-        await user.edit(f"Auto Name has been started my Master")
-        await bot.send_message(Config.LOGGER_ID, "#AUTONAME \n\nAutoname Started!!")
+
 
 
 @Andencento.on(admin_cmd(pattern="autobio"))  # pylint:disable=E0602
