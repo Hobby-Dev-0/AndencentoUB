f40be9c442d978742236dacbb67ee6801968b7f0
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-12 14:39:35 +0530

    _helper

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -1,36 +1,34 @@
-  
-from . import CMD_LIST
+from userbot import CMD_LIST, SUDO_LIST
+from userbot import ALIVE_NAME
+from platform import uname
+import sys
+from telethon import events, functions, __version__
 
-@command(pattern="^.ehelp ?(.*)")
+from . import *
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "cat"
+
+@command(pattern="^.help ?(.*)")
 async def cmd_list(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        tgbotusername = Config.BOT_USERNAME
+        tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
         input_str = event.pattern_match.group(1)
         if tgbotusername is None or input_str == "text":
             string = ""
             for i in CMD_LIST:
-                string += "ℹ️ " + i + "\n"
+                string += "👉 " + i + "\n"
                 for iter_list in CMD_LIST[i]:
                     string += "    `" + str(iter_list) + "`"
                     string += "\n"
                 string += "\n"
             if len(string) > 4095:
-                with io.BytesIO(str.encode(string)) as out_file:
-                    out_file.name = "cmd.txt"
-                    await bot.send_file(
-                        event.chat_id,
-                        out_file,
-                        force_document=True,
-                        allow_cache=False,
-                        caption="**COMMANDS**",
-                        reply_to=reply_to_id
-                    )
-                    await event.delete()
+                await borg.send_message(event.chat_id, "Do .help cmd")
+                await asyncio.sleep(5)
             else:
                 await event.edit(string)
         elif input_str:
             if input_str in CMD_LIST:
-                string = "Commands found in {}:".format(input_str)
+                string = "Commands found in {}:\n".format(input_str)
                 for i in CMD_LIST[input_str]:
                     string += "    " + i
                     string += "\n"
@@ -38,8 +36,8 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """Userbot Helper.. \nProvided by Andencentouserbot\n
-`Userbot Helper to reveal all the commands`"""
+            help_string = f"""Userbot Helper.. Provided by {DEFAULTUSER} \n
+Userbot Helper to reveal all the commands\n__Do `.help` plugin_name for commands, in case popup doesn't appear.__\nDo `.info` plugin_name for usage"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 help_string
@@ -50,3 +48,11 @@ async def cmd_list(event):
                 hide_via=True
             )
             await event.delete()
+
+@borg.on(admin_cmd(pattern="dc"))  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    result = await borg(functions.help.GetNearestDcRequest())  # pylint:disable=E0602
+    await event.edit(result.stringify())
+
