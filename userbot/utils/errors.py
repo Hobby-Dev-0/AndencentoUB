48bb9f0ce136e75a5f5284c9693a085018193f7f
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-12 14:18:55 +0530

    eror

--- userbot/utils/errors.py
+++ userbot/utils/errors.py
@@ -63,7 +63,7 @@ def errors_handler(func):
 
             ftext += "\n\n\nLast 5 commits:\n"
 
-            process = await asyncio.create_subprocess_sEiva(
+            process = await asyncio.create_subprocess_shell(
                 command,
                 stdout=asyncio.subprocess.PIPE,
                 stderr=asyncio.subprocess.PIPE)
