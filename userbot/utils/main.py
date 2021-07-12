dfa1533a4d5e48bdd954d7f4dd05ec14368d260f
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-12 15:03:43 +0530

    F

--- userbot/utils/main.py
+++ userbot/utils/main.py
@@ -62,6 +62,7 @@ def load_module(shortname):
         # support for paperplaneextended
         sys.modules["userbot.mainfiles.events"] = userbot.utils
         spec.loader.exec_module(mod)
+        sys.modules["userbot.plugins." + shortname] = mod
         # for imports
 
 def remove_plugin(shortname):
