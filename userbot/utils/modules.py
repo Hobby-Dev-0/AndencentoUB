20131cdddbac2896f86f42a761df74dd04053a2c
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-07-13 09:27:13 +0530

    Update modules.py

--- userbot/utils/modules.py
+++ userbot/utils/modules.py
@@ -124,7 +124,7 @@ def extra(shortname):
         import userbot.utils
 
         path = Path(f"Addons-Andencento/{shortname}.py")
-        name = "Addons-Andencento.{}".format(shortname)
+        name = "Addons-Andencento.plugins.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
         mod.Andencento = Andencento
