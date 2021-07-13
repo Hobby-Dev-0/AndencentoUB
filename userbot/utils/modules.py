ae79d7bec9b0a19fa0a9e23a641338bda3f2bbab
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-07-13 10:01:04 +0530

    mod

--- userbot/utils/modules.py
+++ userbot/utils/modules.py
@@ -178,7 +178,77 @@ def extra(shortname):
         sys.modules["Addons-Andencento." + shortname] = mod
         LOGS.info("Addons-Andencento imported " + shortname)
 
+cdrepo = Config.REPO_CD
 
+def extra_util(shortname):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        import userbot.utils
+
+        path = Path(f"Addons-Andencento/{shortname}.py")
+        name = "Addons-Andencento.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        LOGS.info("Successfully imported " + shortname)
+    else:
+        import userbot.utils
+
+        path = Path(f"{cdrepo}/{shortname}.py")
+        name = f"{cdrepo}.plugins.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.Andencento = Andencento
+        mod.bot = Andencento
+        mod.delete_hell = delete
+        mod.eod = delete
+        mod.admin_cmd = admin_cmd
+        mod.Var = Var
+        mod.command = command
+        mod.logger = logging.getLogger(shortname)
+        mod.extremepro_cmd = admin_cmd
+        mod.amanpandey_cmd = sudo_cmd
+        mod.LOGS = LOGS
+        mod.tgbot = Andencento.tgbot
+        mod.sudo_cmd = sudo_cmd
+        sys.modules["userbot"] = userbot
+        sys.modules["userbot.utils"] = userbot.utils
+        sys.modules["Extre.events"] = userbot.utils
+        sys.modules["userbot.events"] = userbot.utils
+        sys.modules["ULTRA.utils"] = userbot.utils
+        sys.modules["userbot.Config"] = userbot.config
+        sys.modules["userbot.uniborConfig"] = userbot.config
+        sys.modules["ub"] = userbot
+        sys.modules["jarvis"] = userbot
+        sys.modules["support"] = userbot
+        sys.modules["userbot"] = userbot
+        sys.modules["telebot"] = userbot
+        sys.modules["fridaybot"] = userbot
+        sys.modules["jarvis.utils"] = userbot.utils
+        sys.modules["uniborg.util"] = userbot.utils
+        sys.modules["telebot.utils"] = userbot.utils
+        sys.modules["userbot.utils"] = userbot.utils
+        sys.modules["userbot.events"] = userbot.utils
+        sys.modules["jarvis.jconfig"] = userbot.config
+        sys.modules["userbot.config"] = userbot.config
+        sys.modules["fridaybot.utils"] = userbot.utils
+        sys.modules["fridaybot.Config"] = userbot.config
+        sys.modules["userbot.uniborgConfig"] = userbot.config
+        mod.edit_or_reply = edit_or_reply
+        mod.logger = logging.getLogger(shortname)
+        # support for uniborg
+        sys.modules["uniborg.util"] = userbot.utils
+        mod.Config = Config
+        mod.borg = Andencento
+        mod.edit_or_reply = edit_or_reply
+        mod.eor = edit_or_reply
+        # support for paperplaneextended
+        sys.modules["userbot.mainfiles.events"] = userbot.utils
+        spec.loader.exec_module(mod)
+        # for imports
+        sys.modules[f"{cdrepo}." + shortname] = mod
+        LOGS.info(f"{cdrepo} imported " + shortname)
 def remove_plugin(shortname):
     try:
         try:
