64512bd328c376849b1f3cc89851aa3153eb5b18
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-14 14:37:31 +0530

    Update var.py

--- var.py
+++ var.py
@@ -3,6 +3,7 @@ import os
 ENV = bool(os.environ.get("ENV", False))
 if ENV:
     from config import Var as Config
+    from Config import Var as Config
 else:
     from local_config import Development as Config
 
