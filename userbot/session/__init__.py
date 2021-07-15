c17a2d5b4fdb6ad885bc50ebfd2632b989c3937f
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-07-15 18:57:49 +0530

    Update __init__.py

--- userbot/session/__init__.py
+++ userbot/session/__init__.py
@@ -1,7 +1,1 @@
-from .. import *
-from ..utils import *
-from ..config import *
-from ..helpers import *
-from ..random_strings import *
-from .session import Andencento
-from .session import Andencento as bot
+from .session import *
