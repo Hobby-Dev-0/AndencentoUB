becb9058901400d70d9bdcb2f15f6dc238abc81f
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-15 11:38:32 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -5,7 +5,6 @@ import sys
 from pathlib import Path
 
 import telethon.utils
-from telethon import TelegramClient
 from telethon.tl.functions.channels import InviteToChannelRequest, JoinChannelRequest
 from config import Config
 from . import *
