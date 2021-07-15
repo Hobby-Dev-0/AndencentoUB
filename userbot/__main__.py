932a479cae0bcf2c4ede68a9c19fa48516ce2ed7
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-15 11:37:41 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -14,8 +14,8 @@ from .utils.modules import extra
 hl = Config.HANDLER
 PIC = Config.ALIVE_PIC or "https://telegra.ph/file/3d208ecf6d0ea9389d8f8.jpg"
 ForGo10God = Andencento.uid
-Andencento_USER = Andencento.me.first_name
-Andencento_mention = f"[{Andencento_USER}](tg://user?id={ForGo10God})"
+ALIVE = Config.YOUR_NAME or "ANDENCENTO USER"
+Andencento_mention = f"[{ALIVE}](tg://user?id={ForGo10God})"
 user_mention = Andencento_mention
 ver = "0.0.1"
 # let's get the bot ready
