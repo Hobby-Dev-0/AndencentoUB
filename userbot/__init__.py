26586a5dc44e1783913741717da2b5e0e25d0ebb
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-15 15:28:00 +0530

    loop

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -23,6 +23,7 @@ from config import Config
 from config import Config as Var
 ALIVE_NAME = Config.YOUR_NAME
 StartTime = time.time()
+YOUR_NAME = Config.YOUR_NAME
 from Config import Config
 versionop = "0.0.2"
 W2Hversion = versionop
