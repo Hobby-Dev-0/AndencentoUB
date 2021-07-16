41a71b5f6e3e7362031eb4383347af750201b032
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-16 15:40:37 +0530

    extra

--- userbot/utils/extras.py
+++ userbot/utils/extras.py
@@ -113,3 +113,28 @@ async def delete(event, text, time=None, parse_mode=None, link_preview=None):
         )
     await asyncio.sleep(time)
     return await event.delete()
+
+
+
+
+
+async def edit_delete(event, text, time=None, parse_mode=None, link_preview=None):
+    sudo_users = _sudousers_list()
+    parse_mode = parse_mode or "md"
+    link_preview = link_preview or False
+    time = time or 5
+    if event.sender_id in sudo_users:
+        reply_to = await event.get_reply_message()
+        catevent = (
+            await reply_to.reply(text, link_preview=link_preview, parse_mode=parse_mode)
+            if reply_to
+            else await event.reply(
+                text, link_preview=link_preview, parse_mode=parse_mode
+            )
+        )
+    else:
+        event = await event.edit(
+            text, link_preview=link_preview, parse_mode=parse_mode
+        )
+    await asyncio.sleep(time)
+    return await event.delete()
