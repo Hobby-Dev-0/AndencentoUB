ab789e73fe7133bee1ee64a4e6083120d7c4a446
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-17 12:10:04 +0530

    op

--- userbot/assistant/bot_alive.py
+++ userbot/assistant/bot_alive.py
@@ -1,19 +1,18 @@
 from telethon import events
-
+from .. import ver
 from userbot import ALIVE_NAME, bot
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/3d208ecf6d0ea9389d8f8.jpg"
+PM_IMG = os.environ.get("ALIVE_PIC", None) or "https://telegra.ph/file/3d208ecf6d0ea9389d8f8.jpg"
 pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
 pm_caption += "➥ **SYSTEMS STATS**\n"
 pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
 pm_caption += "➥ **Python:** `3.9.6` \n"
 pm_caption += "➥ **Database Status:**  `Functional`\n"
 pm_caption += "➥ **Current Branch** : `master`\n"
-pm_caption += f"➥ **Version** : `2.0`\n"
+pm_caption += f"➥ **Version** : `{ver}`\n"
 pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
-pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "➥ **License** : [GNU Affero General Public License v3.0](https://github.com/Noob-Stranger/andencento/blob/master/LICENSE)\n"
+pm_caption += f"➥ **License** : [GNU Affero General Public License v3.0](https://github.com/Noob-Stranger/andencento/blob/master/LICENSE)\n"
 pm_caption += (
     "➥ **Copyright** : By [Andencento](https://github.com/Noob-Stranger/andencento)\n"
 )
