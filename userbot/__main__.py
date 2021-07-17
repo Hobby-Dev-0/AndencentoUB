97d97b43670e2faf982fe56c8cc2f3164ceb7b2d
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-17 09:37:20 +0530

    _main

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -10,44 +10,14 @@ from config import Config
 from . import *
 from .utils import *
 from .session.main import *
-hl = Config.HANDLER
-PIC = Config.ALIVE_PIC or "https://telegra.ph/file/3d208ecf6d0ea9389d8f8.jpg"
-ALIVE = Config.YOUR_NAME or "ANDENCENTO USER"
-Andencento_mention = f"[{ALIVE}]"
-user_mention = Andencento_mention
-ver = "0.0.2"
-# let's get the bot ready
-async def Andencento_bot(bot_token):
-    try:
-        await Andencento.start(bot_token)
-        Andencento.me = await Andencento.get_me()
-        Andencento.uid = telethon.utils.get_peer_id(Andencento.me)
-    except Exception as e:
-        LOGS.error(f"ANDENCENTO_SESSION - {str(e)}")
-        sys.exit()
 
-
-# Andencento bot starter...
-if len(sys.argv) not in (1, 3, 4):
-    Andencento.disconnect()
-else:
-    Andencento.tgbot = None
-    try:
-        if Config.BOT_USERNAME is not None:
-            LOGS.info("Checking Telegram Bot Username...")
-            Andencento.tgbot = TelegramClient(
-                "BOT_TOKEN", api_id=Config.APP_ID, api_hash=Config.API_HASH
-            ).start(bot_token=Config.BOT_TOKEN)
-            LOGS.info("Checking Completed. Proceeding to next step...")
-            LOGS.info(" Starting Andencento")
-            Andencento.loop.run_until_complete(Andencento_bot(Config.BOT_USERNAME))
-            LOGS.info(" Andencento Startup Completed")
-        else:
-            Andencento.start()
-    except Exception as e:
-        LOGS.error(f"BOT_TOKEN - {str(e)}")
-        sys.exit()
-       
+try:
+    print ("Configuring Envoirment")
+    Andencento.loop.run_until_complete(botstarted())
+    LOGS.info("Envoirment is configured for bot")
+except Exception as e:
+    LOGS.error(f"{str(e)}")
+    sys.exit()
 
 async def mod():
     await asst()
