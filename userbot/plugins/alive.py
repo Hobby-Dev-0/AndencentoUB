d9259d95f4f38803e4341f30915a7ef708918af7
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-17 12:25:49 +0530

    alive

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,26 +1,26 @@
-import asyncio
-import os
 from telethon import events
-from telethon.tl.types import ChannelParticipantsAdmins
-from platform import uname
-from userbot import ALIVE_NAME
-from userbot.utils import admin_cmd
-PIC = os.environ.get("ALIVE_PIC", None) or "https://telegra.ph/file/3d208ecf6d0ea9389d8f8.jpg"
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Andencento User"
+from .. import ver
+import os
+from userbot import YOUR_NAME as ALIVE_NAME, bot
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
+PM_IMG = os.environ.get("ALIVE_PIC", None) or "https://telegra.ph/file/3d208ecf6d0ea9389d8f8.jpg"
+pm_caption = "➥ **αη∂єη¢єηтσ:** `ONLINE`\n\n"
+pm_caption += "➥ **ѕуѕтємѕ ѕтαтѕ**\n"
+pm_caption += "➥ **тєℓєтнση νєяѕιση:** `1.15.0` \n"
+pm_caption += "➥ **ρутнση:** `3.9.6` \n"
+pm_caption += "➥ **∂αтαвαѕє ѕтαтυѕ:**  `Functional`\n"
+pm_caption += "➥ **¢υяяєηт вяαη¢н** : `master`\n"
+pm_caption += f"➥ **νєяѕιση** : `{ver}`\n"
+pm_caption += f"➥ **му вσѕѕ** : {DEFAULTUSER} \n"
+pm_caption += f"➥ **ℓι¢єηѕє** : [𝘎𝘕𝘜 𝘈𝘧𝘧𝘦𝘳𝘰 𝘎𝘦𝘯𝘦𝘳𝘢𝘭 𝘗𝘶𝘣𝘭𝘪𝘤 𝘓𝘪𝘤𝘦𝘯𝘴𝘦 𝘷3.0](https://github.com/Noob-Stranger/andencento/blob/master/LICENSE)\n"
+pm_caption += (
+    "➥ **¢σρуяιgнт** : By [𝘛𝘦𝘢𝘮 𝘈𝘯𝘥𝘦𝘯𝘤𝘦𝘯𝘵𝘰](https://github.com/Noob-Stranger/andencento)\n"
+)
 
 
-TEXT = f"`Currently I am Working Fine my peru master!` **Andencento Userbot**\n"
-TEXT = f"`Telethon version:` 1.23.0\n"
-TEXT = f"`Python:` 3.9.6\n"
-TEXT = f"`Bot created by:` [Noob-Stranger](tg://user?id=1725374070), @NoobStrangerPerson\n"
-TEXT = f"`My peru owner`: {DEFAULTUSER}\n"
-        
+# only Owner Can Use it
 @Andencento.on(admin_cmd(outgoing=True, pattern="alive$"))
 @Andencento.on(sudo_cmd(pattern="alive$", allow_sudo=True))
-async def up(op):
-    if op.fwd_from:
-        return
-    await op.get_chat()
-    await op.delete()
-    await borg.send_file(op.chat_id, PIC, caption=TEXT)
-    await op.delete() 
+async def _(event):
+    await Andencento.send_file(event.chat_id, PM_IMG, caption=pm_caption)
