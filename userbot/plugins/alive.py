eff412d843db1df67dd40d1883dc92dedabf2452
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-17 11:46:49 +0530

    alive

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -8,12 +8,15 @@ from userbot.utils import admin_cmd
 PIC = os.environ.get("ALIVE_PIC", None) or "https://telegra.ph/file/3d208ecf6d0ea9389d8f8.jpg"
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Andencento User"
 
-@command(outgoing=True, pattern="^.alive$")
+
 TEXT = "`Currently I am Working Fine my peru master!` **Andencento Userbot**\n\n"
                      "`Telethon version: 1.23.0\nPython: 3.9.6\n`"
                      "`Bot created by:` [Noob-Stranger](tg://user?id=1725374070), @NoobStrangerPerson\n"
                      f"`My peru owner`: {DEFAULTUSER}\n\n"
                      "https://github.com/Noob-Stranger/andencento"
+        
+@Andencento.on(admin_cmd(outgoing=True, pattern="alive$"))
+@Andencento.on(sudo_cmd(pattern="alive$", allow_sudo=True))
 async def up(op):
     if op.fwd_from:
         return
