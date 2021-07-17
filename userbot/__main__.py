335e7ad7e9c45c6fbfc656f8495dd79425665d6a
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-17 10:29:15 +0530

    session

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -18,11 +18,28 @@ ALIVE = Config.YOUR_NAME or "ANDENCENTO USER"
 Andencento_mention = f"[{ALIVE}]"
 user_mention = Andencento_mention
 ver = "0.0.2"
-# let's get the bot ready
-async def stop():
-    await main()
+# let's get the bot ready                    
+async def add_bot(bot_token):
+    await bot.start(bot_token)
+    Andencento.me = await bot.get_me() 
+    Andencento.uid = telethon.utils.get_peer_id(bot.me)
 
-Andencento.loop.run_until_complete(stop())
+if len(argv) not in (1, 3, 4):
+    Andencento.disconnect()
+else:
+    Andencento.tgbot = None
+    if Config.BOT_TOKEN is not None:
+        print("CHECKING BOT USERNAME")
+        # ForTheGreatrerGood of beautification
+        Andencento.tgbot = TelegramClient(
+            "TG_BOT_TOKEN",
+            api_id=Var.APP_ID,
+            api_hash=Var.API_HASH
+        ).start(bot_token=Var.BOT_TOKEN)
+        Andencento.loop.run_until_complete(add_bot(Var.BOT_TOKEN))
+        print("CHECKING SUCESS")
+    else:
+        Andencento.start()
 
 async def mod():
     await asst()
@@ -31,23 +48,7 @@ async def mod():
 
 
 Andencento.loop.run_until_complete(mod())
-
-async def op():
-    await Andencento(JoinChannelRequest("Andencento"))
-    await Andencento(JoinChannelRequest("AndencentoSupport"))
-        
 Andencento.loop.create_task(op())
-
-async def Andencentoiosop():
-    try:
-        if Config.LOGGER_ID != 0:
-            await bot.tgbot.send_file(
-                Config.LOGGER_ID,
-                PIC,
-                caption=f"#START \n\nDeployed Andencento Successfully\n\n**Andencento - {ver}**\n\nType `{hl}ping` or `{hl}alive` to check! \n\nJoin [Andencneto Channel](t.me\n\n /Andencento) for Updates & [Andencento Chat](t.me/AndencentoSupport) for any query regarding Team Andencento",
-            )
-    except Exception as e:
-        LOGS.info(str(e))
 
 
 
