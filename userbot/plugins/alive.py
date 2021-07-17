db33b907e30c367ee368adc580eadea61600449e
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-17 11:44:57 +0530

    r

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -9,10 +9,15 @@ PIC = os.environ.get("ALIVE_PIC", None) or "https://telegra.ph/file/3d208ecf6d0e
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Andencento User"
 
 @command(outgoing=True, pattern="^.alive$")
-async def amireallyalive(alive):
-    """ For .alive command, check if the bot is running.  """
-    await borg.send_file(alive.chat_id, PIC, "`Currently Alive, my peru master!` **Andencento Userbot**\n\n"
+TEXT = "`Currently I am Working Fine my peru master!` **Andencento Userbot**\n\n"
                      "`Telethon version: 1.23.0\nPython: 3.9.6\n`"
                      "`Bot created by:` [Noob-Stranger](tg://user?id=1725374070), @NoobStrangerPerson\n"
                      f"`My peru owner`: {DEFAULTUSER}\n\n"
-                     "https://github.com/Noob-Stranger/andencento")
+                     "https://github.com/Noob-Stranger/andencento"
+async def up(op):
+    if op.fwd_from:
+        return
+    await op.get_chat()
+    await op.delete()
+    await borg.send_file(op.chat_id, PIC, caption=TEXT)
+    await op.delete() 
