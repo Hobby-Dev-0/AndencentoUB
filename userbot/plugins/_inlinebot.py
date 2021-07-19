f7af008be31c37bd682cfdd4ce0fce65dd0ddb9b
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-19 10:11:14 +0530

    _inline siple

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -1,231 +1,117 @@
-import os
-import re
-import urllib
+  
 from math import ceil
-from re import findall
-from youtube_search import YoutubeSearch
-from search_engine_parser import GoogleSearch
-from urllib.parse import quote
-import requests
-from telethon import Button, custom, events, functions
-from .. import ALIVE_NAME, CMD_HELP, CMD_LIST
-from .. import *
-PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
-if PMPERMIT_PIC is None:
-    WARN_PIC = "https://telegra.ph/file/3d208ecf6d0ea9389d8f8.jpg"
-else:
-    WARN_PIC = PMPERMIT_PIC
-LOG_CHAT = Config.PRIVATE_GROUP_ID
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Andencento"
+import asyncio
+import json
+import random
+import re
+from telethon import events, errors, custom
+from userbot import CMD_LIST
+from config import Config
+import io
 
-
-@tgbot.on(events.InlineQuery)
-async def inline_handler(event):
-    builder = event.builder
-    result = None
-    query = event.text
-    if event.query.user_id == bot.uid and query.startswith("Andencento"):
-        rev_text = query[::-1]
-        buttons = paginate_help(0, CMD_HELP, "helpme")
-        result = builder.article(
-            "© Userbot Help",
-            text="{}\nCurrently Loaded Plugins: {}".format(query, len(CMD_LIST)),
-            buttons=buttons,
-            link_preview=False,
-        )
-        await event.answer([result])
-    elif event.query.user_id == bot.uid and query == "stats":
-        result = builder.article(
-            title="Stats",
-            text=f"**Showing Stats For {DEFAULTUSER}'s Andencento** \nNote --> Only Owner Can Check This \n(C) @Andencento",
-            buttons=[
-                [custom.Button.inline("Show Stats ?", data="terminator")],
-                [Button.url("Repo 🇮🇳", "https://github.com/Noob-Stranger/andencento")],
-                [Button.url("Join Channel ❤️", "t.me/Andencento")],
-            ],
-        )
-        await event.answer([result])
-    elif event.query.user_id == bot.uid and query.startswith("**Hello"):
-        result = builder.photo(
-            file=WARN_PIC,
-            text=query,
-            buttons=[
-                [custom.Button.inline("Spamming", data="dontspamnigga")],
-                [
-                    custom.Button.inline(
-                        "Casual Talk",
-                        data="whattalk",
-                    )
-                ],
-                [custom.Button.inline("Requesting", data="askme")],
-            ],
-        )
-        await event.answer([result])
-
-
-@tgbot.on(
-    events.callbackquery.CallbackQuery(  # pylint:disable=E0602
+if Config.BOT_USERNAME is not None and tgbot is not None:
+    @tgbot.on(events.InlineQuery)  # pylint:disable=E0602
+    async def inline_handler(event):
+        builder = event.builder
+        result = None
+        query = event.text
+        if event.query.user_id == bot.uid and query.startswith("Userbot"):
+            rev_text = query[::-1]
+            buttons = paginate_help(0, CMD_LIST, "helpme")
+            result = builder.article(
+                "© Andencento-UserBot Help",
+                text="{}\n🔢 Currently Loaded Plugins: {}".format(
+                    query, len(CMD_LIST)),
+                buttons=buttons,
+                link_preview=False
+            )
+        await event.answer([result] if result else None)
+    @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
         data=re.compile(b"helpme_next\((.+?)\)")
-    )
-)
-async def on_plug_in_callback_query_handler(event):
-    if event.query.user_id == bot.uid:
-        current_page_number = int(event.data_match.group(1).decode("UTF-8"))
-        buttons = paginate_help(current_page_number + 1, CMD_HELP, "helpme")
-        # https://t.me/TelethonChat/115200
-        await event.edit(buttons=buttons)
-    else:
-        reply_popp_up_alert = "Please get your own Userbot, and don't use mine!"
-        await event.answer(reply_popp_up_alert, cache_time=0, alert=True)
+    ))
+    async def on_plug_in_callback_query_handler(event):
+        if event.query.user_id == bot.uid:  # pylint:disable=E0602
+            current_page_number = int(
+                event.data_match.group(1).decode("UTF-8"))
+            buttons = paginate_help(
+                current_page_number + 1, CMD_LIST, "helpme")
+            # https://t.me/TelethonChat/115200
+            await event.edit(buttons=buttons)
+        else:
+            reply_pop_up_alert = "Check Pinned Message in\n@ANDENCENTO And\nGet Your Own Userbot"
+            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
 
-@tgbot.on(
-    events.callbackquery.CallbackQuery(  # pylint:disable=E0602
+    @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
         data=re.compile(b"helpme_prev\((.+?)\)")
-    )
-)
-async def on_plug_in_callback_query_handler(event):
-    if event.query.user_id == bot.uid:  # pylint:disable=E0602
-        current_page_number = int(event.data_match.group(1).decode("UTF-8"))
-        buttons = paginate_help(
-            current_page_number - 1, CMD_HELP, "helpme"  # pylint:disable=E0602
-        )
-        # https://t.me/TelethonChat/115200
-        await event.edit(buttons=buttons)
-    else:
-        reply_pop_up_alert = "Please get your own Userbot, and don't use mine!"
-        await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-
-
-@tgbot.on(
-    events.callbackquery.CallbackQuery(  # pylint:disable=E0602
+    ))
+    async def on_plug_in_callback_query_handler(event):
+        if event.query.user_id == bot.uid:  # pylint:disable=E0602
+            current_page_number = int(
+                event.data_match.group(1).decode("UTF-8"))
+            buttons = paginate_help(
+                current_page_number - 1,
+                CMD_LIST,  # pylint:disable=E0602
+                "helpme"
+            )
+            # https://t.me/TelethonChat/115200
+            await event.edit(buttons=buttons)
+        else:
+            reply_pop_up_alert = "Check Pinned Message in\n@ANDENCENTO And\nGet Your Own Userbot"
+            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+    @tgbot.on(events.callbackquery.CallbackQuery(  # pylint:disable=E0602
         data=re.compile(b"us_plugin_(.*)")
-    )
-)
-async def on_plug_in_callback_query_handler(event):
-    if not event.query.user_id == bot.uid:
-        sedok = "Who Are You? Get Your Own Andencento."
-        await event.answer(sedok, cache_time=0, alert=True)
-        return
-    plugin_name = event.data_match.group(1).decode("UTF-8")
-    if plugin_name in CMD_HELP:
-        help_string = f"**💡 PLUGIN NAME 💡 :** `{plugin_name}` \n{CMD_HELP[plugin_name]}"
-    reply_pop_up_alert = help_string
-    reply_pop_up_alert += "\n\n**(C) @ANDENCENTO** ".format(plugin_name)
-    if len(reply_pop_up_alert) >= 4096:
-        crackexy = "`Pasting Your Help Menu.`"
-        await event.answer(crackexy, cache_time=0, alert=True)
-        out_file = reply_pop_up_alert
-        url = "https://del.dog/documents"
-        r = requests.post(url, data=out_file.encode("UTF-8")).json()
-        url = f"https://del.dog/{r['key']}"
-        await event.edit(
-            f"Pasted {plugin_name} to {url}",
-            link_preview=False,
-            buttons=[[custom.Button.inline("Go Back", data="backme")]],
-        )
-    else:
-        await event.edit(
-            message=reply_pop_up_alert,
-            buttons=[[custom.Button.inline("Go Back", data="backme")]],
-        )
+    ))
+    async def on_plug_in_callback_query_handler(event):
+        plugin_name = event.data_match.group(1).decode("UTF-8")
+        help_string = ""
+        try:
+            for i in CMD_LIST[plugin_name]:
+                help_string += i
+                help_string += "\n"
+        except:
+            pass
+        if help_string is "":
+            reply_pop_up_alert = "{} is useless".format(plugin_name)
+        else:
+            reply_pop_up_alert = help_string
+        reply_pop_up_alert += "\n Use .unload {} to remove this plugin\n\
+            © DYNAMIC-UserbotBot".format(plugin_name)
+        try:
+            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+        except:
+            with io.BytesIO(str.encode(reply_pop_up_alert)) as out_file:
+                out_file.name = "{}.txt".format(plugin_name)
+                await event.client.send_file(
+                    event.chat_id,
+                    out_file,
+                    force_document=True,
+                    allow_cache=False,
+                    caption=plugin_name
+                )
 
 
-
-    
-@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"dontspamnigga")))
-async def rip(event):
-    if event.query.user_id == bot.uid:
-        sedok = "Master, You Don't Need To Use This."
-        await event.answer(sedok, cache_time=0, alert=True)
-        return
-    await event.get_chat()
-    him_id = event.query.user_id
-    text1 = "You Have Chosed A Probhited Option. Therefore, You Have Been Blocked By UserBot. 🇮🇳"
-    await event.edit("Choice Not Accepted ❌")
-    await borg.send_message(event.query.user_id, text1)
-    await borg(functions.contacts.BlockRequest(event.query.user_id))
-    await borg.send_message(
-            LOG_CHAT,
-            f"Hello, A Noob [Nibba](tg://user?id={him_id}) Selected Probhited Option, Therefore Blocked."
-    )
-
-@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"backme")))
-async def sed(event):
-    if event.query.user_id != bot.uid:
-        sedok = "Who Are You? Get Your Own Andencento."
-        await event.answer(sedok, cache_time=0, alert=True)
-        return
-    await event.answer("Back", cache_time=0, alert=False)
-    # This Is Copy of Above Code. (C) @SpEcHiDe
-    buttons = paginate_help(0, CMD_HELP, "helpme")
-    sed = f"""Andencento Userbot Modules Are Listed Here !\n
-For More Help or Support Visit @Andencento \nCurrently Loaded Plugins: {len(CMD_LIST)}"""
-    await event.edit(message=sed, buttons=buttons)
-
-
-@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"whattalk")))
-async def rip(event):
-    if event.query.user_id == bot.uid:
-        sedok = "Master, You Don't Need To Use This."
-        await event.answer(sedok, cache_time=0, alert=True)
-        return
-    await event.get_chat()
-    him_id = event.query.user_id
-    await event.edit("Choice Accepted ✔️")
-    text2 = "Ok. Please Wait Until My Master Approves. Don't Spam Or Try Anything Stupid. \nThank You For Contacting Me."
-    await borg.send_message(event.query.user_id, text2)
-    await borg.send_message(
-        LOG_CHAT,
-        message=f"Hello, A [New User](tg://user?id={him_id}). Wants To Talk With You.")
-
-
-@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"askme")))
-async def rip(event):
-    if event.query.user_id == bot.uid:
-        sedok = "Master, You Don't Need To Use This."
-        await event.answer(sedok, cache_time=0, alert=True)
-        return
-    await event.get_chat()
-    him_id = event.query.user_id
-    await event.edit("Choice Accepted ✔️")
-    text3 = "Ok, Wait. You can Ask After Master Approves You. Kindly, Wait."
-    await borg.send_message(event.query.user_id, text3)
-    await borg.send_message(
-        LOG_CHAT,
-        message=f"Hello, A [New User](tg://user?id={him_id}). Wants To Ask You Something.")
-
-
-def paginate_help(page_number, loaded_modules, prefix):
+def paginate_help(page_number, loaded_plugins, prefix):
     number_of_rows = 8
     number_of_cols = 2
-    helpable_modules = []
-    for p in loaded_modules:
+    helpable_plugins = []
+    for p in loaded_plugins:
         if not p.startswith("_"):
-            helpable_modules.append(p)
-    helpable_modules = sorted(helpable_modules)
-    modules = [
-        custom.Button.inline(
-            "{} {} {}".format(" ", x, " "), data="us_plugin_{}".format(x)
-        )
-        for x in helpable_modules
-    ]
-    pairs = list(zip(modules[::number_of_cfols], modules[1::number_of_cols]))
+            helpable_plugins.append(p)
+    helpable_plugins = sorted(helpable_plugins)
+    modules = [custom.Button.inline(
+        "{} {}".format(" ", x),
+        data="us_plugin_{}".format(x))
+        for x in helpable_plugins]
+    pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
     if len(modules) % number_of_cols == 1:
         pairs.append((modules[-1],))
     max_num_pages = ceil(len(pairs) / number_of_rows)
     modulo_page = page_number % max_num_pages
     if len(pairs) > number_of_rows:
-        pairs = pairs[
-            modulo_page * number_of_rows : number_of_rows * (modulo_page + 1)
-        ] + [
-            (
-                custom.Button.inline(
-                    "Previous", data="{}_prev({})".format(prefix, modulo_page)
-                ),
-                custom.Button.inline(
-                    "Next", data="{}_next({})".format(prefix, modulo_page)
-                ),
-            )
+        pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
+            [
+            (custom.Button.inline("<<Previous", data="{}_prev({})".format(prefix, modulo_page)),
+             custom.Button.inline("Next>>", data="{}_next({})".format(prefix, modulo_page)))
         ]
     return pairs
