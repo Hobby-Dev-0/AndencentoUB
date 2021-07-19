d257f24e7ff9e6042455172081d621d5e060adc1
Noob Stranger <85062690+Noob-Stranger@users.noreply.github.com>
2021-07-19 09:29:25 +0530

    pmpermit

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -137,7 +137,7 @@ if Var.PRIVATE_GROUP_ID is not None:
         chat_ids = event.sender_id
         if USER_BOT_NO_WARN == message_text:
             return
-        sender = await bot(GetFullUserRequest(event.sender_id))
+        sender = event.sender_id
         if chat_ids == bot.uid:
             return
         if sender.user.bot:
